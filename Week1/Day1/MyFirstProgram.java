34774810a390082efb724a2e14fcf7e9ff5e605a
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-04-15 17:17:45 +0530

    Add files via upload

---
+++ Week1/Day1/MyFirstProgram.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class MyFirstProgram {
+             //Identifier	
+	// main method
+	public static void main(String[] args) {
+		// console model : ip /op
+		//         Input Stream / output Stream 
+		
+		// ctrl+ space --> content assist for windows
+		// command + space --> content assist for ios
+		//""(double quotes) --> string
+		// not using any quote --> integer
+		//''(singlge quotes)--> character
+		
+		
+		System.out.println("Hello World");
+		System.out.println(56);	
+		System.out.println('H');
+         
+	}
+	
+	
+
+}
